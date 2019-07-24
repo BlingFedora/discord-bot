33c270a4839ca6b165a9d2e062e57c1d6d740766
BlingFedora <53269952+BlingFedora@users.noreply.github.com>
2019-07-24 16:55:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.NTg0MjQ4NzgwMzA0NDE2ODA4.XTh_MA.xtDN7ebwYSaLlbLjMIUK2k42mVg);//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
