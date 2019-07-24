8d65cabb621e26fe21ad9c297c64388197a5ebd6
BlingFedora <53269952+BlingFedora@users.noreply.github.com>
2019-07-24 17:05:10 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,8 +27,3 @@ client.on('message', message => {
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
-
-client.user.setActivity("with depressing lifes", {
-  type: "STREAMING",
-  url: "https://www.twitch.tv/monstercat"
-});
